391875291b97bad94779ff0dc4565331a67bb00b
Enderyasli <enderyasli@gmail.com>
2021-05-29 03:02:54 +0300

    image select and crop

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -1,3 +1,6 @@
+
+-keep class androidx.appcompat.widget.** { *; }
+
 # Add project specific ProGuard rules here.
 # You can control the set of applied configuration files using the
 # proguardFiles setting in build.gradle.
--- app/src/main/java/com/pawtind/android/ui/main/view/login/singUp/AddImageFragment.kt
+++ app/src/main/java/com/pawtind/android/ui/main/view/login/singUp/AddImageFragment.kt
@@ -1,14 +1,25 @@
 package com.pawtind.android.ui.main.view.login.singUp
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.pawtind.android.R
+import com.bumptech.glide.Glide
+import com.pawtind.android.databinding.FragmentAddImageBinding
 import com.pawtind.android.ui.base.BaseFragment
+import com.theartofdev.edmodo.cropper.CropImage
+import kotlinx.android.synthetic.main.fragment_add_image.*
+import kotlinx.android.synthetic.main.item_layout.view.*
+
 
 class AddImageFragment : BaseFragment() {
+
+    override var bottomNavigationViewVisibility = View.GONE
+    private var _binding: FragmentAddImageBinding? = null
+
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,10 +29,39 @@ class AddImageFragment : BaseFragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_image, container, false)
+    ): View {
+        _binding = FragmentAddImageBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.imageView.setOnClickListener {
+
+            // for fragment (DO NOT use `getActivity()`)
+            CropImage.activity()
+                .setAspectRatio(1,1)
+                .start(requireContext(), this)
+        }
+
+
+        return view
+
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            val result = CropImage.getActivityResult(data)
+            if (resultCode == -1) {
+                val resultUri: Uri = result.uri
+                Glide.with(requireContext())
+                    .load(resultUri)
+                    .into(binding.imageView)
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                val error = result.error
+            }
+        }
     }
 
-
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
